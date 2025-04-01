d03a7e0c8e9180781e97b519a19f5a08eb816143
thecatzpajamas <189032626+thecatzpajamas@users.noreply.github.com>
2025-03-31 21:28:10 -0400

    Create custom_module.py

---
+++ Python/Time Travel Calculator/custom_module.py
@@ -0,0 +1,3 @@
+# A module to be imported as a proof of concept
+def generate_time_travel_message(year, destination, cost):
+  return print("A trip to {year} {destination} would cost roughly {cost}!".format(year=year, destination=destination, cost=cost))
